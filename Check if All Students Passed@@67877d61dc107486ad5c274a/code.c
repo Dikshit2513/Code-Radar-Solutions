a68b470366ad04828c300d6e6d34671668c255c3
Dikshit2513 <dikshit1792.becse24@chitkara.edu.in>
2025-03-11 17:44:48 +0530

    Solution to Check if All Students Passed

--- Check if All Students Passed@@67877d61dc107486ad5c274a/code.c
+++ Check if All Students Passed@@67877d61dc107486ad5c274a/code.c
@@ -1,39 +1,30 @@
 #include <stdio.h>
-
 struct Student {
-    int roll_number;
+    int rollNumber;
     char name[50];
     float marks;
 };
 
 int main() {
-    int N;
-    int allPassed = 1; // Flag to check if all students passed
+    int n, allpassed = 1;
+    scanf("%d", &n);
+    struct Student students[n];
     
-    // Input the number of students
-    printf("Enter the number of students: ");
-    scanf("%d", &N);
-    
-    // Create an array of Student structures
-    struct Student students[N];
-    
-    // Input data for each student
-    for (int i = 0; i < N; i++) {
-        printf("Enter roll number, name, and marks for student %d: ", i + 1);
-        scanf("%d %s %f", &students[i].roll_number, students[i].name, &students[i].marks);
-        
-        // Check if marks are below the passing threshold
-        if (students[i].marks < 50) {
-            allPassed = 0; // If any student fails, set flag to 0
-        }
+    for (int i = 0; i < n; i++) {
+        scanf("%d %s %f", &students[i].rollNumber, students[i].name, &students[i].marks);
     }
 
-    // Output result
-    if (allPassed) {
-        printf("All Passed\n");
-    } else {
-        printf("Not All Passed\n");
+    for (int i = 0; i < n; i++) {
+        if(students[i].marks < 50 ){
+            allpassed = 0;
+        }
     }
+     if (allpassed) {
+            printf("All Passed");
+        }
+        else{
+            printf("Not All Passed");
+        }
 
     return 0;
 }
