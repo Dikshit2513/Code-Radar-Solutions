6a5c0469a9b12deacf57136428d54385b86be7b6
Dikshit2513 <dikshit1792.becse24@chitkara.edu.in>
2025-03-11 17:39:45 +0530

    Solution to Search Student by Roll Number

--- Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
+++ Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
@@ -2,7 +2,7 @@
 
 #define MAX_STUDENTS 100
 
-// Structure to hold student details
+
 struct Student {
     int roll_number;
     char name[50];
@@ -14,18 +14,17 @@ int main() {
     struct Student students[MAX_STUDENTS];
     float total_marks = 0.0;
 
-    // Input number of students
+    
     printf("Enter the number of students: ");
     scanf("%d", &N);
 
-    // Input student details
     for (i = 0; i < N; i++) {
         printf("Enter roll number, name, and marks for student %d: ", i + 1);
         scanf("%d %s %f", &students[i].roll_number, students[i].name, &students[i].marks);
-        total_marks += students[i].marks;  // Aggregate total marks
+        total_marks += students[i].marks;  
     }
 
-    // Calculate and display average marks
+    
     float average_marks = total_marks / N;
     printf("Average Marks: %.2f\n", average_marks);
 
